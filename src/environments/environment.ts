a91cc08847e927f8087d5845377a15ecc3a4118d
Deepak <deepak@relinns.com>
2024-12-07 18:49:04 +0530

    Initial commit

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'http://localhost:1337'
+  // baseUrl: 'http://localhost:1337'
+  baseUrl: 'strapi-backend-production-5fb0.up.railway.app'
 };
